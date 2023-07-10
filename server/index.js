586025d2aea1966d8d1380879e1f735adfa14f16
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-07-10 17:20:10 -0400

    react frontend

--- server/index.js
+++ server/index.js
@@ -15,6 +15,8 @@ const app = express();
 //database connection
 connectDB();
 
+app.use(cors());
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -25,7 +27,6 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
