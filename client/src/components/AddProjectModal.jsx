59f75e27fba1083e41036a0166007653c767ebe0
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-07-11 16:29:27 -0400

    delete projects

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { FaList } from "react-icons/fa";
-import { useQuery } from "@apollo/client";
-
+import { useQuery, useMutation } from "@apollo/client";
+import { ADD_PROJECT } from "../mutations/projectMutations";
+import { GET_PROJECTS } from "../queries/projectQueries";
 import { GET_CLIENTS } from "../queries/clientQueries";
 
 export default function AddProjectModal() {
@@ -9,6 +10,17 @@ export default function AddProjectModal() {
   const [description, setDescription] = useState("");
   const [clientId, setClientId] = useState("");
   const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
 
   // Get Clients for select
   const { loading, error, data } = useQuery(GET_CLIENTS);
@@ -19,6 +31,8 @@ export default function AddProjectModal() {
     if (name === "" || description === "" || status === "") {
       return alert("Please fill in all fields");
     }
+
+    addProject(name, description, clientId, status);
 
     setName("");
     setDescription("");
